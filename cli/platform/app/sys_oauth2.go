8fc1a684d1823247d88294ceea06725cfdc33d5d
2637309949@qq.com <2637309949@qq.com>
2020-01-16 17:29:39 +0800

    fix: SetCookieName

--- cli/platform/app/sys_oauth2.go
+++ cli/platform/app/sys_oauth2.go
@@ -23,10 +23,10 @@ import (
 func init() {
 	var hashKey = []byte("FF51A553-72FC-478B-9AEF-93D6F506DE91")
 	session.InitManager(
-		session.SetCookieName("sid"),
+		// session.SetCookieName("session_id"),
 		session.SetStore(
 			cookie.NewCookieStore(
-				cookie.SetCookieName("ssid"),
+				cookie.SetCookieName("session_store_id"),
 				cookie.SetHashKey(hashKey),
 			),
 		),
