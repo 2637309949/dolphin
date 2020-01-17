25c66fb47353ca798a96ea53f3f2f0e26c21606f
2637309949@qq.com <2637309949@qq.com>
2020-01-17 11:56:42 +0800

    fix: _

--- cli/gen/tempalte/tool.go
+++ cli/gen/tempalte/tool.go
@@ -20,13 +20,11 @@ var defaultLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY
 // RandomString returns a random string with a fixed length
 func RandomString(n int, allowedChars ...[]rune) string {
 	var letters []rune
-
 	if len(allowedChars) == 0 {
 		letters = defaultLetters
 	} else {
 		letters = allowedChars[0]
 	}
-
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
--- srv/demo/go.mod
+++ srv/demo/go.mod
@@ -5,7 +5,6 @@ go 1.13
 replace github.com/2637309949/dolphin => ../../
 
 require (
-	github.com/2637309949/bulrush-utils v0.0.0-20191021061903-25a27153dbb0
 	github.com/2637309949/dolphin v0.0.0-00010101000000-000000000000
 	github.com/go-sql-driver/mysql v1.4.1
 )
