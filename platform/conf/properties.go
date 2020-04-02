617c317d0e3a1395a6142f7d168a3c953f77e4a9
2637309949@qq.com <2637309949@qq.com>
2020-04-02 17:14:07 +0800

    style: conf风格修改

--- platform/conf/properties.go
+++ platform/conf/properties.go
@@ -12,6 +12,8 @@ func init() {
 	viper.SetConfigName("app")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("conf")
+	viper.SetDefault("app.name", "dolphin")
+	viper.SetDefault("app.version", "1.0")
 	viper.SetDefault("app.mode", "release")
 	viper.SetDefault("app.viper", false)
 	viper.SetDefault("http.hash", "FF61A573-82FC-478B-9AEF-93D6F506DE9A")
@@ -26,10 +28,19 @@ func init() {
 	viper.SetDefault("db.driver", "mysql")
 	viper.SetDefault("db.dataSource", "root:111111@/dolphin?charset=utf8&parseTime=True&loc=Local")
 	viper.SetDefault("rd.dataSource", ":@127.0.0.1:6379/0")
+	viper.SetDefault("dir.app", "app")
+	viper.SetDefault("dir.doc", "doc")
 	viper.SetDefault("dir.sql", "sql")
 	viper.SetDefault("dir.sqlmap", "sqlmap")
 	viper.SetDefault("dir.script", "script")
 	viper.SetDefault("dir.log", "log")
+	viper.SetDefault("dir.util", "util")
+	viper.SetDefault("dir.model", "model")
+	viper.SetDefault("dir.srv", "srv")
+	viper.SetDefault("oauth.id", "Y76U9344RABF4")
+	viper.SetDefault("oauth.secret", "98UYO6FVB865")
+	viper.SetDefault("oauth.login", "/static/web/login.html")
+	viper.SetDefault("oauth.affirm", "/static/web/affirm.html")
 	viper.SetDefault("swag.license.name", "Apache 2.0")
 	viper.SetDefault("swag.license.url", "http://www.apache.org/licenses/LICENSE-2.0.html")
 	viper.SetDefault("swag.securitydefinitions.oauth2.accessCode", "OAuth2AccessCode")
@@ -47,6 +58,9 @@ func init() {
 	}
 	if strings.TrimSpace(viper.GetString("oauth.cli")) == "" {
 		viper.SetDefault("oauth.cli", fmt.Sprintf("http://127.0.0.1:%v", viper.GetString("http.port")))
+	}
+	if strings.TrimSpace(viper.GetString("host")) == "" {
+		viper.SetDefault("host", fmt.Sprintf("127.0.0.1:%v", viper.GetString("http.port")))
 	}
 	if viper.GetBool("app.viper") {
 		if err := viper.WriteConfig(); err != nil {
