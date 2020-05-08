71979a19989e27ad7b4fbae2cd383054a703db25
double <2637309949@qq.com>
2020-05-08 16:53:37 +0800

    fix: dolphin

--- cmd/dolphin/parser/xml.go
+++ cmd/dolphin/parser/xml.go
@@ -321,7 +321,7 @@ func (parser *AppParser) Walk(xmlPath string) error {
 		go 1.13
 
 		require (
-			github.com/2637309949/dolphin v0.0.0-20200508084502-42b0468dd150
+			github.com/2637309949/dolphin v0.0.0-20200508085117-55eaef5adf58
 			github.com/go-sql-driver/mysql v1.5.0
 			google.golang.org/grpc v1.26.0
 		)`))
