7bdf41305cd2481b7088e24dacb47f76f645541b
double <2637309949@qq.com>
2020-06-27 11:38:29 +0800

    feat: worker Unmarshal

--- platform/model/worker.helper.go
+++ platform/model/worker.helper.go
@@ -3,9 +3,31 @@
 
 package model
 
+import (
+	"encoding/json"
+	"github.com/2637309949/dolphin/packages/logrus"
+)
+
 const (
 	WorkerStatusInitial = iota + 100
 	WorkerStatusProccessing
 	WorkerStatusInterrupt
 	WorkerStatusFinish
 )
+
+// TableName table name of defined SysUser
+func (m *Worker) Unmarshal(v interface{}) error {
+	var (
+		bs  []byte
+		err error
+	)
+	if bs, err = json.Marshal(m.Payload); err != nil {
+		logrus.Error(err)
+		return err
+	}
+	if err := json.Unmarshal(bs, v); err != nil {
+		logrus.Error(err)
+		return err
+	}
+	return nil
+}
