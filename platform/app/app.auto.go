41f6153341873734bcecdc875f8843a4d20bf9d3
double.huang <2637309949@qq.com>
2020-10-13 15:58:07 +0800

    style: _

--- platform/app/app.auto.go
+++ platform/app/app.auto.go
@@ -8,7 +8,6 @@ import (
 	"github.com/2637309949/dolphin/platform/rpc"
 	"github.com/2637309949/dolphin/platform/rpc/proto"
 
-	"github.com/2637309949/dolphin/packages/logrus"
 	"github.com/2637309949/dolphin/packages/viper"
 	"github.com/2637309949/dolphin/platform/util"
 )
@@ -1145,6 +1144,6 @@ var Executor = util.NewExecutor(SyncModel, SyncCtr, SyncService)
 
 func init() {
 	if err := Executor.Execute(); err != nil {
-		logrus.Fatal(err)
+		panic(err)
 	}
 }
