c45adb66bbe87771c936e4f93a02c39dad0e49c7
double.huang <2637309949@qq.com>
2021-03-24 17:58:16 +0800

    style: 调整格式

--- scripts/k3s.sh
+++ scripts/k3s.sh
@@ -2,4 +2,4 @@ sudo curl -sfL https://get.k3s.io |INSTALL_K3S_SKIP_DOWNLOAD=true K3S_KUBECONFIG
 kubectl delete -f /var/lib/rancher/k3s/server/manifests/traefik.yaml
 kubectl apply -f ./kong-ingress.yaml
 docker run -p 1337:1337 -e "TOKEN_SECRET=723rebwher982h3rh239rbb8hvysuh" -e "NODE_ENV=production" --name konga pantsel/konga
-sudo docker run --privileged -d -v /home/double/App/docker/data/rancher:/var/lib/rancher -p 8888:80 -p 8443:443 rancher/rancher
+docker run --privileged -d -v /home/double/App/docker/data/rancher:/var/lib/rancher -p 8888:80 -p 8443:443 rancher/rancher
