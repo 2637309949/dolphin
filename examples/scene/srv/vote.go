dd35a28b84078874deba229b3b39f8fd215f88ea
double.huang <2637309949@qq.com>
2021-04-09 21:57:55 +0800

    style: 调整代码风格

--- examples/scene/srv/vote.go
+++ examples/scene/srv/vote.go
@@ -27,40 +27,49 @@ func VoteLike(ctx *gin.Context, db *xorm.Engine, params model.VoteInfo) (interfa
 	}
 	// 2.添加点赞,仅仅判断缓存级别的
 	// post用户集合 点赞+取消 为了查找key
-	if _, err := app.RedisClient.SAdd(context.Background(), fmt.Sprintf("post_user_like_set_%v", params.PostId.String), params.UserId.String).Result(); err != nil {
+	if _, err := app.RedisClient.SAdd(context.Background(),
+		fmt.Sprintf("post_user_like_set_%v", params.PostId.String), params.UserId.String).Result(); err != nil {
 		return nil, err
 	}
 	// post用户集合 点赞
 	if params.Type.Int64 == 101 {
 		// 统计文章点赞数目
-		if _, err := app.RedisClient.SAdd(context.Background(), fmt.Sprintf("post_user_like_set_incr_%v", params.PostId.String), params.UserId.String).Result(); err != nil {
+		if _, err := app.RedisClient.SAdd(context.Background(),
+			fmt.Sprintf("post_user_like_set_incr_%v", params.PostId.String), params.UserId.String).Result(); err != nil {
 			return nil, err
 		}
 		// 用户查找列表时, 需要判断该文章是否已经点赞
-		if _, err := app.RedisClient.SAdd(context.Background(), fmt.Sprintf("user_post_like_set_%v", params.UserId.String), params.PostId.String).Result(); err != nil {
+		if _, err := app.RedisClient.SAdd(context.Background(),
+			fmt.Sprintf("user_post_like_set_%v", params.UserId.String), params.PostId.String).Result(); err != nil {
 			return nil, err
 		}
 	} else if params.Type.Int64 == 102 {
 		// 统计文章点赞数目
-		if _, err := app.RedisClient.SRem(context.Background(), fmt.Sprintf("post_user_like_set_incr_%v", params.PostId.String), params.UserId.String).Result(); err != nil {
+		if _, err := app.RedisClient.SRem(context.Background(),
+			fmt.Sprintf("post_user_like_set_incr_%v", params.PostId.String), params.UserId.String).Result(); err != nil {
 			return nil, err
 		}
 		// 用户查找列表时, 需要判断该文章是否已经点赞
-		if _, err := app.RedisClient.SRem(context.Background(), fmt.Sprintf("user_post_like_set_%v", params.UserId.String), params.PostId.String).Result(); err != nil {
+		if _, err := app.RedisClient.SRem(context.Background(),
+			fmt.Sprintf("user_post_like_set_%v", params.UserId.String), params.PostId.String).Result(); err != nil {
 			return nil, err
 		}
 	}
 	// 3.写入post_user_like_{$post_id}{$user_id}
-	if _, err := app.RedisClient.HMSet(context.Background(), fmt.Sprintf("post_user_like_%v%v", params.PostId.String, params.UserId.String), "create_time", time.Now().Value().Format("2006-01-02 15:04"), "type", params.Type.Int64).Result(); err != nil {
+	if _, err := app.RedisClient.HMSet(context.Background(),
+		fmt.Sprintf("post_user_like_%v%v", params.PostId.String, params.UserId.String),
+		"create_time", time.Now().Value().Format("2006-01-02 15:04"), "type", params.Type.Int64).Result(); err != nil {
 		return nil, err
 	}
 	// 4.新增计数
-	num, err := app.RedisClient.SCard(context.Background(), fmt.Sprintf("post_user_like_set_incr_%v", params.PostId.String)).Result()
+	num, err := app.RedisClient.SCard(context.Background(),
+		fmt.Sprintf("post_user_like_set_incr_%v", params.PostId.String)).Result()
 	if err != nil {
 		logrus.Error(err)
 		return nil, err
 	}
-	if _, err := app.RedisClient.Set(context.Background(), fmt.Sprintf("post%v_counter", num), params.PostId.String, 0).Result(); err != nil {
+	if _, err := app.RedisClient.Set(context.Background(),
+		fmt.Sprintf("post%v_counter", num), params.PostId.String, 0).Result(); err != nil {
 		return nil, err
 	}
 	return "ok", nil
@@ -81,19 +90,22 @@ func init() {
 				logrus.Error(err)
 				break
 			}
-			num2, err := app.RedisClient.SCard(context.Background(), fmt.Sprintf("post_user_like_set_%v", postId)).Result()
+			num2, err := app.RedisClient.SCard(context.Background(),
+				fmt.Sprintf("post_user_like_set_%v", postId)).Result()
 			if err != nil {
 				logrus.Error(err)
 				break
 			}
 			for i := int64(0); i < num2; i++ {
 				ulp := model.UserLikePost{}
-				userId, err := app.RedisClient.SPop(context.Background(), fmt.Sprintf("post_user_like_set_%v", postId)).Result()
+				userId, err := app.RedisClient.SPop(context.Background(),
+					fmt.Sprintf("post_user_like_set_%v", postId)).Result()
 				if err != nil {
 					logrus.Error(err)
 					break
 				}
-				field, err := app.RedisClient.HMGet(context.Background(), fmt.Sprintf("post_user_like_%v%v", postId, userId), "create_time", "type").Result()
+				field, err := app.RedisClient.HMGet(context.Background(),
+					fmt.Sprintf("post_user_like_%v%v", postId, userId), "create_time", "type").Result()
 				if err != nil {
 					logrus.Error(err)
 					break
@@ -113,11 +125,13 @@ func init() {
 				} else if tpe == 102 {
 					ulpsDecr = append(ulpsDecr, ulp)
 				}
-				if _, err := app.RedisClient.Del(context.Background(), fmt.Sprintf("post_user_like_%v%v", postId, userId)).Result(); err != nil {
+				if _, err := app.RedisClient.Del(context.Background(),
+					fmt.Sprintf("post_user_like_%v%v", postId, userId)).Result(); err != nil {
 					logrus.Error(err)
 					break
 				}
-				if _, err := app.RedisClient.Decr(context.Background(), fmt.Sprintf("post%v_counter", postId)).Result(); err != nil {
+				if _, err := app.RedisClient.Decr(context.Background(),
+					fmt.Sprintf("post%v_counter", postId)).Result(); err != nil {
 					logrus.Error(err)
 					break
 				}
@@ -177,7 +191,8 @@ func init() {
 				}
 				items := funk.Filter(v, func(v1 model.UserLikePost) bool {
 					for i3 := range extList {
-						if v1.UserId.String == extList[i3].UserId.String && v1.PostId.String == extList[i3].PostId.String {
+						if v1.UserId.String == extList[i3].UserId.String &&
+							v1.PostId.String == extList[i3].PostId.String {
 							return false
 						}
 					}
