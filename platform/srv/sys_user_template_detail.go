69a73fa633ce8409e1d196951b599e39aaf62df9
double.huang <2637309949@qq.com>
2021-06-02 14:28:00 +0800

    feat: 调整TODO

--- platform/srv/sys_user_template_detail.go
+++ platform/srv/sys_user_template_detail.go
@@ -17,18 +17,18 @@ import (
 func SysUserTemplateDetailTODO(ctx *gin.Context, db *xorm.Engine, params struct{}) (interface{}, error) {
 	cwt, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
 	chi := func(cwt context.Context) chan interface{} {
 		chi := make(chan interface{}, 1)
 		go func() {
 			time.Sleep(1 * time.Second)
 			chi <- 100
-			close(chi)
 			cancel()
+			close(chi)
 		}()
 		return chi
 	}(cwt)
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
 	for range ticker.C {
 		select {
 		case <-cwt.Done():
