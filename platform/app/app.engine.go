81935959e17fe4d176fb0501c3ae4899bb9b9bf1
double.huang <2637309949@qq.com>
2021-06-03 17:08:41 +0800

    style: nothing

--- platform/app/app.engine.go
+++ platform/app/app.engine.go
@@ -129,10 +129,8 @@ func (e *Engine) database() {
 	e.PlatformDB.SetMaxOpenConns(viper.GetInt("db.maxOpenConns"))
 	e.RegisterSQLDir(e.PlatformDB, path.Join(".", viper.GetString("dir.sql")))
 	e.RegisterSQLMap(e.PlatformDB, sql.SQLTPL)
-	{
-		new(model.SysDomain).Ensure(e.PlatformDB)
-		new(model.SysDomain).InitSysData(e.PlatformDB.NewSession())
-	}
+	new(model.SysDomain).Ensure(e.PlatformDB)
+	new(model.SysDomain).InitSysData(e.PlatformDB.NewSession())
 
 	// initBusinessDB
 	asyncOnce, domains := sync.Once{}, []model.SysDomain{}
