1186e438302195ef889f58b805656f5028cd1860
2637309949 <2637309949@qq.com>
2021-07-30 11:37:34 +0800

    fix: XlsxImport

--- examples/scene/api/xlsx.go
+++ examples/scene/api/xlsx.go
@@ -5,8 +5,10 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
+	"github.com/2637309949/dolphin/platform/util/math"
 	"github.com/sirupsen/logrus"
 )
 
@@ -39,6 +41,21 @@ func (ctr *Xlsx) XlsxImport(ctx *Context) {
 	}
 
 	sheet, err := ctr.Srv.ParseExcel(file, 1)
+	for i := range sheet {
+		item := sheet[i]
+		for k, v := range item {
+			switch k {
+			case "已录收单":
+				fmt.Println("已录收单:", math.MustInt(v))
+			case "无效收单":
+				fmt.Println("无效收单:", math.MustInt(v))
+			case "有效收单":
+				fmt.Println("有效收单:", math.MustInt(v))
+			case "日期":
+				fmt.Println("日期:", math.MustTime(v, "2006-01-02"))
+			}
+		}
+	}
 	if err != nil {
 		logrus.Error(err)
 		ctx.Fail(err)
--- platform/util/math/must.go
+++ platform/util/math/must.go
@@ -1,1 +1,60 @@
 package math
+
+import (
+	"strconv"
+	"time"
+)
+
+// MustInt64 defined TODO
+func MustInt64(x string) int64 {
+	i, _ := strconv.ParseInt(x, 10, 64)
+	return i
+}
+
+// MustInt32 defined TODO
+func MustInt32(x string) int32 {
+	i, _ := strconv.ParseInt(x, 10, 16)
+	return int32(i)
+}
+
+// MustInt16 defined TODO
+func MustInt16(x string) int16 {
+	i, _ := strconv.ParseInt(x, 10, 16)
+	return int16(i)
+}
+
+// MustInt8 defined TODO
+func MustInt8(x string) int8 {
+	i, _ := strconv.ParseInt(x, 10, 8)
+	return int8(i)
+}
+
+// MustInt defined TODO
+func MustInt(x string) int {
+	i, _ := strconv.ParseInt(x, 10, 0)
+	return int(i)
+}
+
+// MustFloat32 defined TODO
+func MustFloat32(x string) float32 {
+	i, _ := strconv.ParseFloat(x, 32)
+	return float32(i)
+}
+
+// MustFloat64 defined TODO
+func MustFloat64(x string) float64 {
+	i, _ := strconv.ParseFloat(x, 64)
+	return float64(i)
+}
+
+// MustBool defined TODO
+func MustBool(x string) bool {
+	i, _ := strconv.ParseBool(x)
+	return i
+}
+
+// MustTime defined TODO
+func MustTime(x string, layout string) time.Time {
+	i, _ := time.Parse(layout, x)
+	return i
+}
